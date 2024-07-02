f0b653da2e5ca78e69998507086f4040b86d0373
Enric Cervera <ecervera@uji.es>
2024-07-02 17:23:48 +0200

    Use ZED 2 model

--- run.sh
+++ run.sh
@@ -6,6 +6,6 @@ docker run --gpus all -it --privileged --ipc=host --pid=host \
   -e NVIDIA_DRIVER_CAPABILITIES=all -e DISPLAY \
   -v /dev:/dev -v /tmp/.X11-unix/:/tmp/.X11-unix \
   zed-ros2-examples:latest \
-  ros2 launch zed_display_rviz2 display_zed_cam.launch.py camera_model:=zed
+  ros2 launch zed_display_rviz2 display_zed_cam.launch.py camera_model:=zed2
   
 xhost -si:localuser:root
