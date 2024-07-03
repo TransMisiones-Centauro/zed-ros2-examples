4708d8e52c129028467a26abfce740b5b123530f
Enric Cervera <ecervera@uji.es>
2024-07-03 10:25:13 +0200

    Mount folder for AI models

--- run.sh
+++ run.sh
@@ -5,6 +5,7 @@ xhost +si:localuser:root
 docker run --gpus all -it --privileged --ipc=host --pid=host \
   -e NVIDIA_DRIVER_CAPABILITIES=all -e DISPLAY \
   -v /dev:/dev -v /tmp/.X11-unix/:/tmp/.X11-unix \
+  -v ~/Desktop/CENTAURO/Perception/zed_data:/usr/local/zed/resources \
   zed-ros2-examples:latest \
   ros2 launch zed_display_rviz2 display_zed_cam.launch.py camera_model:=zed2
   
