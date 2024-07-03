dcac28bcdf2aa7bda28b201d38ba551ac515adfe
Enric Cervera <ecervera@uji.es>
2024-07-03 11:07:10 +0200

    Run with GPU and X11

--- run_bash.sh
+++ run_bash.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
-docker run --rm -it --privileged --ipc=host --pid=host \
+xhost +si:localuser:root
+
+docker run --gpus all -it --privileged --ipc=host --pid=host \
+  -e NVIDIA_DRIVER_CAPABILITIES=all -e DISPLAY \
+  -v /dev:/dev -v /tmp/.X11-unix/:/tmp/.X11-unix \
+  -v ~/Desktop/CENTAURO/Perception/zed_data:/usr/local/zed/resources \
   zed-ros2-examples:latest \
   bash
-
+  
+xhost -si:localuser:root
